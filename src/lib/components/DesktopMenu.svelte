d3745532686707deba3cd4390371f1ad89f6babf
Daniele Cencig <daniele.cencig@outlook.com>
2023-06-03 22:01:07 +0200

    Adds links to navigate

--- src/lib/components/DesktopMenu.svelte
+++ src/lib/components/DesktopMenu.svelte
@@ -1,1 +1,7 @@
-<aside class="h-screen hidden md:block w-80 bg-primary p-4 pt-8 duration-300">Palestrina</aside>
+<script lang="ts">
+	import Button from './ui/button/Button.svelte';
+</script>
+
+<aside class="h-screen hidden md:block w-80 bg-primary px-4 py-8 duration-300">
+	<Button class="text-white" variant="link" href="/account">Palestrina</Button>
+</aside>
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
+	import Button from '$src/lib/components/ui/button/Button.svelte';
 
 	export let data;
 
 	if (!data.userProfile.has_compiled) {
 		if (browser) {
-			throw goto(`/account/${data.userProfile.id}/profileEdit?new=true`);
+			goto(`/account/${data.userProfile.id}/profileEdit?new=true`);
 		}
 	}
 </script>
 
 <h1>Account page {JSON.stringify(data.userProfile)}</h1>
+<Button variant="link" href={`/account/${data.userProfile.id}/profileEdit`}
+	>Vai al tuo profilo</Button
+>
