1794317ef693b843ca838926f7e30d515190689f
Daniele Cencig <daniele.cencig@outlook.com>
2023-06-03 17:41:49 +0200

    File input functionalities

--- src/routes/account/[userId]/profileEdit/+page.svelte
+++ src/routes/account/[userId]/profileEdit/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import SuperDebug from 'sveltekit-superforms/client/SuperDebug.svelte';
-
 	import { page } from '$app/stores';
 
 	import Button from '$src/lib/components/ui/button/Button.svelte';
@@ -11,8 +9,17 @@
 	export let data;
 	const { supabase, avatarPath } = data;
 
+	let files: FileList;
 	let avatarFallback = '';
 	let avatarSrc = '';
+
+	const previewAvatar = async (e: Event) => {
+		const inputElement = e.target as HTMLInputElement;
+		if (inputElement.files && inputElement.files.length !== 0) {
+			const avatar = inputElement.files[0];
+			avatarSrc = URL.createObjectURL(avatar);
+		}
+	};
 
 	const downloadAvatar = async () => {
 		try {
@@ -48,43 +55,32 @@
 	{/if}
 </h1>
 
-<div class="mt-12">
-	<form
-		method="post"
-		class="flex w-full flex-col space-y-8 md:mx-auto md:max-w-5xl"
-		use:enhance
-		enctype="multipart/form-data"
-	>
-		<div class="flex flex-col">
-			<label for="username">Username</label>
-			<Input class="mt-1" type="text" name="username" bind:value={$form.username} />
-			{#if $errors.username}
-				<small class="font-semibold text-red-400">{$errors.username}</small>
-			{/if}
+<form
+	method="post"
+	class="flex w-full flex-col space-y-8 md:mx-auto md:max-w-5xl mt-6"
+	use:enhance
+	enctype="multipart/form-data"
+>
+	<div class="flex flex-col">
+		<label for="username">Username</label>
+		<Input class="mt-1" type="text" name="username" bind:value={$form.username} />
+		{#if $errors.username}
+			<small class="font-semibold text-red-400">{$errors.username}</small>
+		{/if}
+	</div>
+	<div class="flex flex-col">
+		<label for="full_name">Nome</label>
+		<Input type="text" name="full_name" bind:value={$form.full_name} />
+		{#if $errors.full_name}
+			<small class="font-semibold text-red-400">{$errors.full_name}</small>
+		{/if}
+	</div>
+	<div class="flex flex-col">
+		<label for="full_name">Avatar</label>
+		<div class="mt-4 flex items-center gap-x-4">
+			<Avatar src={avatarSrc} username={$form.username ?? ''} fallback={avatarFallback} />
+			<Input type="file" name="avatar" accept="image/*" on:change={previewAvatar} />
 		</div>
-		<div class="flex flex-col">
-			<label for="full_name">Nome</label>
-			<Input type="text" name="full_name" bind:value={$form.full_name} />
-			{#if $errors.full_name}
-				<small class="font-semibold text-red-400">{$errors.full_name}</small>
-			{/if}
-		</div>
-		<div class="flex flex-col">
-			<label for="full_name">Avatar</label>
-			<div class="mt-4 flex items-center gap-x-4">
-				<Avatar
-					src={avatarSrc}
-					{supabase}
-					username={$form.username ?? ''}
-					fallback={avatarFallback}
-				/>
-				<Input type="file" name="avatar" value="avatar" accept="image/*" />
-			</div>
-		</div>
-		<Button type="submit">Salva</Button>
-	</form>
-</div>
-
-<div class="absolute bottom-4 left-[50%] w-[1200px] translate-x-[-50%]">
-	<SuperDebug data={$form} />
-</div>
+	</div>
+	<Button type="submit">Salva</Button>
+</form>
--- src/routes/account/[userId]/profileEdit/Avatar.svelte
+++ src/routes/account/[userId]/profileEdit/Avatar.svelte
@@ -2,9 +2,7 @@
 	import Avatar from '$src/lib/components/ui/avatar/Avatar.svelte';
 	import AvatarFallback from '$src/lib/components/ui/avatar/AvatarFallback.svelte';
 	import AvatarImage from '$src/lib/components/ui/avatar/AvatarImage.svelte';
-	import type { SupabaseClient } from '@supabase/supabase-js';
 
-	export let supabase: SupabaseClient;
 	export let username = '';
 	export let src = '';
 	export let fallback = '';
